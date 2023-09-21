3ad9d9f4d3dd1950fc1a8d65cab82ccb4560aed0
skbestcyber <131454105+skbestcyber@users.noreply.github.com>
2023-09-21 19:26:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['919074692450', 'ABHISHEK-SURESH', true],
+  ['+94 78 649 4127', '༒⃤SAHAN ㋡KESHARA☬࿐', true],
   [''],
   [''] 
 ] //Number of owners
@@ -47,15 +47,15 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = '𝙰𝙱𝙷𝙸𝚂𝙷𝙴𝙺-𝚂𝙴𝚁'
+global.botname = '༒⃤SAHAN ㋡KESHARA☬࿐'
 global.premium = 'true'
-global.packname = '𝙰𝙱𝙷𝙸𝚂𝙷𝙴𝙺-𝚂𝙴𝚁🎯𝙰𝙱𝙷𝙸𝚂𝙷𝙴𝙺-𝚂𝚄𝚁𝙴𝚂𝙷☘️' 
-global.author = 'ABHISHEK SURESH' 
+global.packname = '༒⃤SAHAN ㋡KESHARA☬࿐☘️' 
+global.author = '༒⃤SAHAN ㋡KESHARA☬࿐' 
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/abhishek_ser\n' 
-global.dygp = 'https://chat.whatsapp.com/BOLb0ICN3sAJ5dloRBw5VD'
-global.fgsc = 'https://github.com/AbhishekSuresh2/ABHISHEK-SER' 
-global.fgyt = 'https://youtube.com/@comedymelodych8468'
-global.fgpyp = 'https://youtube.com/@comedymelodych8468'
+global.dygp = 'https://chat.whatsapp.com/Ff9UfufqHAJ5zJB7xHKGxI'
+global.fgsc = 'https://chat.whatsapp.com/Ff9UfufqHAJ5zJB7xHKGxI' 
+global.fgyt = 'https://www.youtube.com/@The_still_word_book'
+global.fgpyp = 'https://www.youtube.com/@The_still_word_book'
 global.fglog = 'https://replicate.delivery/pbxt/QbP6Fh3ZXwKON9SCB70ERGwwgeeSbztwKIOIzhUeXFkwnFHiA/out.png' 
 
 //imagen
